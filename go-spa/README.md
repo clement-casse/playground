b2117209e0b23f01be2fdc22ab9ef525d89fbfa6
Clément Cassé <clement.casse@outlook.com>
2023-10-24 13:21:24 +0200

    Init flake workspace

---
+++ go-spa/README.md
@@ -0,0 +1,2 @@
+# Go Single Page Application
+
---
+++ go-spa/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Definition of a Single-Page-Application with Go backend";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      buildInputs = with pkgs; [
+        # Go and Tools
+        go_1_21
+        gopls
+        gotools
+        go-tools
+
+        # Node & Tools
+        nodejs_20
+        tailwindcss
+      ];
+    in
+    with pkgs;
+    {
+      # formatter: Specify the formatter that will be used by the command `nix fmt`.
+      formatter = nixpkgs-fmt;
+
+      devShells.default = mkShell {
+        inherit buildInputs;
+      };
+    }
+  );
+}
