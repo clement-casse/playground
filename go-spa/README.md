8944b4605df94e13cb497261b05a437bf266c66b
Clément Cassé <clement.casse@outlook.com>
2023-10-24 14:07:41 +0200

    moving template var in a more coherent place And reference in readme

--- go-spa/README.md
+++ go-spa/README.md
@@ -1,2 +1,7 @@
 # Go Single Page Application
 
+## References
+
+- [Xe Iaso Blog post on using Tailwind CSS in Go][1]
+
+[1]: https://xeiaso.net/blog/using-tailwind-go/
--- go-spa/main.go
+++ go-spa/main.go
@@ -27,21 +27,30 @@ func init() {
 	indexTemplate = template.Must(template.ParseFS(templateFS, "templates/index.html"))
 }
 
+// indexTmplVars gather the variables that can be used in the index.html template
+type indexTmplVars struct {
+	Name string
+}
+
 func main() {
 	// Get the values of Command Line Arguments (addr & port)
 	flag.Parse()
 
+	indexVars := &indexTmplVars{
+		Name: "Unknown People",
+	}
+
 	// Define HTTP routes of the Application:
 	// -> Everything under the URI Path `/static/` is a direct mapping of the ./static directory
 	http.Handle("/static/", http.FileServer(http.FS(staticFS)))
-	// -> matches the static page "index.html" and Execute the HTML template before sending the response
+	// -> matches the static page "index" and Execute the HTML template before sending the response
 	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
-		indexTemplate.Execute(w, struct{ Name string }{Name: "Josette"})
+		indexTemplate.Execute(w, indexVars)
 	})
-	// -> Default behaviour, "/" matches everything, therefore reroute the / to index.html and raise 404 elsewhere
+	// -> Default behaviour, "/" matches everything, therefore reroute the / to index and raise 404 elsewhere
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
-			http.Redirect(w, r, "/index", http.StatusPermanentRedirect)
+			http.Redirect(w, r, "/index", http.StatusTemporaryRedirect)
 			return
 		}
 		http.NotFound(w, r)
