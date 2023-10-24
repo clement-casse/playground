80f1d6d77f65866faad0d95335d806f6c5e4f150
Clément Cassé <clement.casse@outlook.com>
2023-10-24 22:40:03 +0200

    Adding a more elaborated API path (just a bit)

--- go-spa/api/router.go
+++ go-spa/api/router.go
@@ -3,21 +3,40 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+)
+
+var (
+	verison   = "0.0.1"
+	startTime = time.Now()
 )
 
 func NewAPIRouter() http.Handler {
 	apiMux := mux.NewRouter()
 
-	apiMux.HandleFunc("/health", healthHandler).Methods(http.MethodGet)
+	// List API routes managed by the API
+	apiMux.Methods(http.MethodGet).Path("/health").HandlerFunc(healthHandler)
+	apiMux.Methods(http.MethodGet).Path("/status").HandlerFunc(statusHandler)
 
-	apiMux.Use(
-		mux.CORSMethodMiddleware(apiMux),
-	)
+	// Set up the HTTP middlewares specifically for the API router
+	apiMux.Use(mux.CORSMethodMiddleware(apiMux))
 	return apiMux
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(&statusPayload{
+		Version: verison,
+		Uptime:  time.Since(startTime),
+	})
+}
+
+type statusPayload struct {
+	Version string        `json:"version"`
+	Uptime  time.Duration `json:"uptime"`
 }
