0776ea2bca7bff943eabe851977a455b8f13a637
Clément Cassé <clement.casse@outlook.com>
2023-10-31 22:28:06 +0100

    cleaning up messy templates

--- go-spa/webapp/App.tsx
+++ go-spa/webapp/App.tsx
@@ -1,25 +1,17 @@
 import { useState } from "react";
 
-export default function() {
+export default function () {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center h-[90vh]">
       <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        count is {count}
+      </button>
+    </div>
   );
 }
