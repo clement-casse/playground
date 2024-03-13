0ebd37811efe1fd61e4d571cbd438282e5f85e00
Clément Cassé <clement.casse@outlook.com>
2024-03-13 15:58:12 +0100

    new cors impl with tests

--- webservice-go/tools/web/cors.go
+++ webservice-go/tools/web/cors.go
@@ -8,7 +8,8 @@ import (
 
 // CORSMiddleware wraps github.com/rs/cors with no additional logic.
 type CORSMiddleware struct {
-	*cors.Cors
+	handler http.Handler
+	cors    *cors.Cors
 }
 
 // NewCORSMiddleware
@@ -16,9 +17,19 @@ func NewCORSMiddleware(allowsOrigins ...string) *CORSMiddleware {
 	if len(allowsOrigins) == 0 {
 		allowsOrigins = []string{"*"}
 	}
-	return &CORSMiddleware{cors.New(cors.Options{AllowedOrigins: allowsOrigins})}
+	return &CORSMiddleware{cors: cors.New(cors.Options{AllowedOrigins: allowsOrigins})}
 }
 
 func (cm *CORSMiddleware) Chain(handler http.Handler) http.Handler {
-	return cm.Handler(handler)
+	cm.handler = handler
+	return cm
+}
+
+func (cm *CORSMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	cm.cors.HandlerFunc(w, r)
+	if cm.cors.OriginAllowed(r) {
+		cm.handler.ServeHTTP(w, r)
+	} else {
+		http.Error(w, "cors", http.StatusNoContent)
+	}
 }
