63de2457923526932022a869a676f5cea7ad4804
Clément Cassé <clement.casse@outlook.com>
2024-03-18 21:39:43 +0100

    aiming 100% coverage

--- webservice-go/tools/web/cidr_protect_test.go
+++ webservice-go/tools/web/cidr_protect_test.go
@@ -19,7 +19,7 @@ func TestNewCIDRProtectMiddleware(t *testing.T) {
 			allowedNetworks: []string{},
 			expectPanic:     false,
 		}, {
-			name:            "come well formated cidr range should not panic",
+			name:            "well formated cidr range should not panic",
 			allowedNetworks: []string{"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", "100.64.0.0/10"},
 			expectPanic:     false,
 		}, {
@@ -55,7 +55,7 @@ func TestCIDRProtectMiddleware(t *testing.T) {
 			remoteAddr:      "8.8.4.4:16781",
 			expectStatus:    http.StatusUnauthorized,
 		}, {
-			name:            "loopback should be allowed enventhough allowedNetworks is empty",
+			name:            "loopback should be allowed eventhough allowedNetworks is empty",
 			allowedNetworks: []string{},
 			remoteAddr:      "127.0.0.1:21345",
 			expectStatus:    http.StatusOK,
@@ -79,6 +79,36 @@ func TestCIDRProtectMiddleware(t *testing.T) {
 			allowedNetworks: []string{"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", "100.64.0.0/10"},
 			remoteAddr:      "8.8.4.4:16781",
 			expectStatus:    http.StatusUnauthorized,
+		}, {
+			name:            "public IPv6 address should not reach inner middleware",
+			allowedNetworks: []string{},
+			remoteAddr:      "[2a01:cb19:85a0:4d00:7016:6357:9140:f300]:16781",
+			expectStatus:    http.StatusUnauthorized,
+		}, {
+			name:            "loopback IPv6 should be allowed eventhough allowedNetworks is empty",
+			allowedNetworks: []string{},
+			remoteAddr:      "[::1]:21345",
+			expectStatus:    http.StatusOK,
+		}, {
+			name:            "allowing IPv6 Unique Local Addressing should allow a local ipv6 address",
+			allowedNetworks: []string{"fc00::/7"},
+			remoteAddr:      "[fc00::abcd]:21345",
+			expectStatus:    http.StatusOK,
+		}, {
+			name:            "allowing IPv6 Unique Local Addressing should disallow not local ipv6 addresses",
+			allowedNetworks: []string{"fc00::/7"},
+			remoteAddr:      "[2a01::abcd]:21345",
+			expectStatus:    http.StatusUnauthorized,
+		}, {
+			name:            "malformed RemoteAddress IP should trigger a 500",
+			allowedNetworks: []string{"fc00::/7"},
+			remoteAddr:      "[2a01::abcdefgh]:21345",
+			expectStatus:    http.StatusInternalServerError,
+		}, {
+			name:            "malformed RemoteAddress field should trigger a 500",
+			allowedNetworks: []string{"fc00::/7"},
+			remoteAddr:      "[2a01::abcd];21345",
+			expectStatus:    http.StatusInternalServerError,
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
