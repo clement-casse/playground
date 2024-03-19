7867e01b8384685b735fb0578e1de91670a057e5
Clément Cassé <clement.casse@outlook.com>
2024-03-19 14:18:50 +0100

    try to add eslint

--- webservice-go/web/app/vite.config.js
+++ webservice-go/web/app/vite.config.js
@@ -3,10 +3,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from 'vite-plugin-checker';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
   plugins: [
     react(),
+    eslint(),
     checker({ typescript: true }),
   ],
   test: {
