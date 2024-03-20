f33d63b1356534b3908e34964a6cee00e0ab00f5
Clément Cassé <clement.casse@outlook.com>
2024-03-20 20:33:57 +0100

    Add HTTP Headers to rate limiter

--- webservice-go/tools/web/ratelimit.go
+++ webservice-go/tools/web/ratelimit.go
@@ -2,10 +2,22 @@ package web
 
 import (
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
 	"golang.org/x/time/rate"
+)
+
+const (
+	// HeaderRateLimitLimit and HeaderRateLimitRemaining are the recommended return header
+	// values from IETF on rate limiting.
+	HeaderRateLimitLimit     = "X-RateLimit-Limit"
+	HeaderRateLimitRemaining = "X-RateLimit-Remaining"
+
+	// HeaderRetryAfter is the header used to indicate when a client should retry
+	// requests (when the rate limit expires), in UTC time.
+	HeaderRetryAfter = "Retry-After"
 )
 
 type clientRateLimiterMiddleware struct {
@@ -13,8 +25,9 @@ type clientRateLimiterMiddleware struct {
 	limitersByClients  map[string]*clientLimiter
 	cleanInterval      time.Duration
 	inactivityDuration time.Duration
-	rateLimit          rate.Limit
+	rateLimit          float64
 	burst              int
+	limitHeader        string
 }
 
 type clientLimiter struct {
@@ -32,8 +45,11 @@ func NewClientRateLimiterMiddleware(rateLimitPerSeconds float64, burst int) Midd
 		limitersByClients:  make(map[string]*clientLimiter),
 		cleanInterval:      1 * time.Minute,
 		inactivityDuration: 10 * time.Minute,
-		rateLimit:          rate.Limit(rateLimitPerSeconds),
+		rateLimit:          rateLimitPerSeconds,
 		burst:              burst,
+
+		// cache the computation of the limit header as a string as the value won't change
+		limitHeader: strconv.FormatInt(int64(burst), 10),
 	}
 	go func() { // periodic cleaning of the limitersbyclients every cleanInterval
 		for {
@@ -52,24 +68,36 @@ func NewClientRateLimiterMiddleware(rateLimitPerSeconds float64, burst int) Midd
 
 func (m *clientRateLimiterMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Use the remote IP to identify the client
 		ip, err := GetRemoteAddr(r)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		clientID := ip.String()
+
+		// Get the correct rate.Limiter for this client
 		m.Lock()
 		if _, found := m.limitersByClients[clientID]; !found {
 			m.limitersByClients[clientID] = &clientLimiter{
-				Limiter: *rate.NewLimiter(m.rateLimit, m.burst),
+				Limiter: *rate.NewLimiter(rate.Limit(m.rateLimit), m.burst),
 			}
 		}
 		m.limitersByClients[clientID].lastSeen = time.Now()
+
 		if !m.limitersByClients[clientID].Allow() {
 			m.Unlock()
+
+			w.Header().Set(HeaderRateLimitLimit, m.limitHeader)
+			w.Header().Set(HeaderRateLimitRemaining, "0")
 			http.Error(w, "rate limit exceeded", http.StatusTooManyRequests)
 			return
 		}
+		remaining := m.limitersByClients[clientID].Tokens()
 		m.Unlock()
+
+		w.Header().Set(HeaderRateLimitLimit, m.limitHeader)
+		w.Header().Set(HeaderRateLimitRemaining, strconv.FormatFloat(remaining, 'f', 0, 64))
+
 		next.ServeHTTP(w, r)
 	})
 }
