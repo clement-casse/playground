d8973e8a8e40fd268724aabe679c7ca1a83ae8ce
Clément Cassé <clement.casse@outlook.com>
2024-03-20 22:40:14 +0100

    feat(webservice-go/web/ratelimiter): Add HTTP Headers & config options (#18)

--- webservice-go/tools/web/ratelimit.go
+++ webservice-go/tools/web/ratelimit.go
@@ -2,19 +2,33 @@ package web
 
 import (
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
 	"golang.org/x/time/rate"
 )
 
+const (
+	// HeaderRateLimitLimit and HeaderRateLimitRemaining are the recommended return header
+	// values from IETF on rate limiting.
+	HeaderRateLimitLimit     = "X-RateLimit-Limit"
+	HeaderRateLimitRemaining = "X-RateLimit-Remaining"
+
+	defaultCleanInterval      = 1 * time.Minute
+	defaultInactivityDuration = 5 * time.Minute
+)
+
 type clientRateLimiterMiddleware struct {
 	sync.Mutex
-	limitersByClients  map[string]*clientLimiter
+	limitersByClients map[string]*clientLimiter
+
+	rateLimit          float64
+	burst              int
 	cleanInterval      time.Duration
 	inactivityDuration time.Duration
-	rateLimit          rate.Limit
-	burst              int
+
+	limitHeader string
 }
 
 type clientLimiter struct {
@@ -22,20 +36,37 @@ type clientLimiter struct {
 	lastSeen time.Time
 }
 
+// RateLimiterOpt in an interface for applying RateLimiterMiddleware options.
+type RateLimiterOpt interface {
+	applyOpt(*clientRateLimiterMiddleware) *clientRateLimiterMiddleware
+}
+
+type rateLimiterOptFunc func(*clientRateLimiterMiddleware) *clientRateLimiterMiddleware
+
+func (fn rateLimiterOptFunc) applyOpt(s *clientRateLimiterMiddleware) *clientRateLimiterMiddleware {
+	return fn(s)
+}
+
 // NewClientRateLimiterMiddleware creates a middleware that will allow clients to make
 // `rateLimitPerSeconds` requests per seconds. For each clients, the middleware creates
 // a "token bucket" limiter of size `burst` which is implemented in "golang.org/x/time/rate".
 // The middleware will cleanup the list of its clients every `cleanInterval` and remove
 // clients inactive for longer than `inactivityDuration`.
-func NewClientRateLimiterMiddleware(rateLimitPerSeconds float64, burst int) Middleware {
+func NewClientRateLimiterMiddleware(rateLimitPerSeconds float64, burst int, opts ...RateLimiterOpt) Middleware {
 	m := &clientRateLimiterMiddleware{
 		limitersByClients:  make(map[string]*clientLimiter),
-		cleanInterval:      1 * time.Minute,
-		inactivityDuration: 10 * time.Minute,
-		rateLimit:          rate.Limit(rateLimitPerSeconds),
+		cleanInterval:      defaultCleanInterval,
+		inactivityDuration: defaultInactivityDuration,
+		rateLimit:          rateLimitPerSeconds,
 		burst:              burst,
+
+		// cache the computation of the limit header as a string as the value won't change
+		limitHeader: strconv.FormatInt(int64(burst), 10),
 	}
-	go func() { // periodic cleaning of the limitersbyclients every cleanInterval
+	for _, opt := range opts {
+		m = opt.applyOpt(m)
+	}
+	go func() { // periodic cleaning of the limitersByClients every cleanInterval
 		for {
 			time.Sleep(m.cleanInterval)
 			m.Lock()
@@ -50,26 +81,56 @@ func NewClientRateLimiterMiddleware(rateLimitPerSeconds float64, burst int) Midd
 	return m
 }
 
+// WithCleanInterval configure a NewRateLimiterMiddleware by setting the cleaning
+// interval to the specified value (by default to 1 minute).
+func WithCleanInterval(d time.Duration) RateLimiterOpt {
+	return rateLimiterOptFunc(func(m *clientRateLimiterMiddleware) *clientRateLimiterMiddleware {
+		m.cleanInterval = d
+		return m
+	})
+}
+
+// WithInactivityDuration configures a NewRateLimiterMiddleware by setting the
+// client inactivity duration to the desired value (by default to 5 minutes).
+func WithInactivityDuration(d time.Duration) RateLimiterOpt {
+	return rateLimiterOptFunc(func(m *clientRateLimiterMiddleware) *clientRateLimiterMiddleware {
+		m.inactivityDuration = d
+		return m
+	})
+}
+
 func (m *clientRateLimiterMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Use the remote IP to identify the client
 		ip, err := GetRemoteAddr(r)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		clientID := ip.String()
+
+		// Get the correct rate.Limiter for this client
 		m.Lock()
 		if _, found := m.limitersByClients[clientID]; !found {
 			m.limitersByClients[clientID] = &clientLimiter{
-				Limiter: *rate.NewLimiter(m.rateLimit, m.burst),
+				Limiter: *rate.NewLimiter(rate.Limit(m.rateLimit), m.burst),
 			}
 		}
 		m.limitersByClients[clientID].lastSeen = time.Now()
+
 		if !m.limitersByClients[clientID].Allow() {
 			m.Unlock()
+
+			w.Header().Set(HeaderRateLimitLimit, m.limitHeader)
+			w.Header().Set(HeaderRateLimitRemaining, "0")
 			http.Error(w, "rate limit exceeded", http.StatusTooManyRequests)
 			return
 		}
+		remaining := m.limitersByClients[clientID].Tokens()
 		m.Unlock()
+
+		w.Header().Set(HeaderRateLimitLimit, m.limitHeader)
+		w.Header().Set(HeaderRateLimitRemaining, strconv.FormatFloat(remaining, 'f', 0, 64))
+
 		next.ServeHTTP(w, r)
 	})
 }
--- webservice-go/tools/web/ratelimit_test.go
+++ webservice-go/tools/web/ratelimit_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,18 +10,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	maxNumberOfRequests                 = 10
+	numberOfRequestRegainedEverySeconds = 5.0
+)
+
+func TestWithCleanInterval(t *testing.T) {
+	interval, err := time.ParseDuration("42s")
+	assert.NoError(t, err)
+	m := NewClientRateLimiterMiddleware(1.0, 1, WithCleanInterval(interval))
+	assert.Equal(t, interval, m.(*clientRateLimiterMiddleware).cleanInterval)
+}
+
+func TestWithInactivityDuration(t *testing.T) {
+	duration, err := time.ParseDuration("42s")
+	assert.NoError(t, err)
+	m := NewClientRateLimiterMiddleware(1.0, 1, WithInactivityDuration(duration))
+	assert.Equal(t, duration, m.(*clientRateLimiterMiddleware).inactivityDuration)
+}
+
 func TestRateLimitMiddleware(t *testing.T) {
-	crlm := NewClientRateLimiterMiddleware(5.0, 10)
+	crlm := NewClientRateLimiterMiddleware(numberOfRequestRegainedEverySeconds, maxNumberOfRequests)
 	testServer := httptest.NewServer(crlm.Handle(testingHandler))
 	defer testServer.Close()
 
 	// Consume a full burst of 10 requests
-	for range 10 {
+	for i := range maxNumberOfRequests {
 		req, err := http.NewRequest(http.MethodGet, testServer.URL, nil)
 		assert.NoError(t, err)
 		res, err := http.DefaultClient.Do(req)
 		assert.NoError(t, err)
 		assert.Equal(t, http.StatusOK, res.StatusCode)
+		assert.Equal(t, "10", res.Header.Get(HeaderRateLimitLimit))
+		assert.Equal(t, fmt.Sprintf("%d", maxNumberOfRequests-i-1), res.Header.Get(HeaderRateLimitRemaining))
 	}
 	// Then the following requests should be discarded with status 429
 	for range 3 {
@@ -29,6 +51,19 @@ func TestRateLimitMiddleware(t *testing.T) {
 		res, err := http.DefaultClient.Do(req)
 		assert.NoError(t, err)
 		assert.Equal(t, http.StatusTooManyRequests, res.StatusCode)
+		assert.Equal(t, "10", res.Header.Get(HeaderRateLimitLimit))
+		assert.Equal(t, "0", res.Header.Get(HeaderRateLimitRemaining))
+	}
+	// making a new client should not limit the requests
+	for i := range 3 {
+		req, err := http.NewRequest(http.MethodGet, testServer.URL, nil)
+		assert.NoError(t, err)
+		req.Header.Set("X-Forwarded-For", "8.8.4.4")
+		res, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+		assert.Equal(t, "10", res.Header.Get(HeaderRateLimitLimit))
+		assert.Equal(t, fmt.Sprintf("%d", maxNumberOfRequests-i-1), res.Header.Get(HeaderRateLimitRemaining))
 	}
 	// Cooldown for regaining 5 requests (rateLimitPerSeconds)
 	time.Sleep(1 * time.Second)
