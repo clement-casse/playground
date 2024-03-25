410e90bcc68ab8e98ccd54f6b8a9a2b8f09f992f
Clément Cassé <clement.casse@outlook.com>
2024-03-25 15:12:53 +0100

    Some empathic comments

--- otelcol-custom/doc.go
+++ otelcol-custom/doc.go
@@ -1,4 +1,8 @@
 //go:generate go run go.opentelemetry.io/collector/cmd/builder --config=./builder-config.yaml
 
-// package defines a demonstration OpenTelemetry Collector with a custom module
+// Package otelcolcustom defines a demonstration OpenTelemetry Collector built with the curent
+// versions of the custom modules that are part of this project.
+//
+// The collector is build with OpenTelemetry Collector Builder (ocb), its components have been
+// defined in the manifest ./builder-config.yaml.
 package otelcolcustom
--- otelcol-custom/tools.go
+++ otelcol-custom/tools.go
@@ -1,6 +1,10 @@
 //go:build tools
 // +build tools
 
+// tools.go serves as a dependedncy management file to bring in this module external go tools.
+// this allows to run ocb and mdatagen without having them installed via //go:generate comments.
+// see https://www.jvt.me/posts/2022/06/15/go-tools-dependency-management/ for more explainations.
+
 package otelcolcustom
 
 import (
