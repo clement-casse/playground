ac93f83cd27635f52a8a61097697d22df65c8f03
Clément Cassé <clement.casse@proton.me>
2024-07-25 13:11:08 +0200

    remove webservice-go githook in nix

--- webservice-go/flake.nix
+++ webservice-go/flake.nix
@@ -4,12 +4,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, pre-commit-hooks }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
+      };
+
+      pre-commit-checks = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          nixpkgs-fmt.enable = true;
+          golangci-lint = {
+            enable = true;
+            language = "golang";
+          };
+        };
       };
 
       buildInputs = with pkgs; [
@@ -26,14 +38,20 @@
         nodejs_20
         typescript
         tailwindcss
-      ];
+      ] ++ pre-commit-checks.enabledPackages;
+
     in
     with pkgs;
     {
       # formatter: Specify the formatter that will be used by the command `nix fmt`.
       formatter = nixpkgs-fmt;
 
+      checks = {
+        pre-commit = pre-commit-checks;
+      };
+
       devShells.default = mkShell {
+        inherit (pre-commit-checks) shellHook;
         inherit buildInputs;
       };
     }
