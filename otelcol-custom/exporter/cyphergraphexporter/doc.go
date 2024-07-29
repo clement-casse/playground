e861a3db378bb9cf5ae0e6eb275c9f30d620f77b
Clément Cassé <clement.casse@proton.me>
2024-07-29 23:58:45 +0200

    remove otelcol custom

--- otelcol-custom/exporter/cyphergraphexporter/doc.go
+++
@@ -1,5 +0,0 @@
-//go:generate go run go.opentelemetry.io/collector/cmd/mdatagen metadata.yaml
-
-// Package cyphergraphexporter implements an OpenTelemetry Collector exporter that sends trace data to
-// a graph database using the Cypher language (Neo4j, Memgraph, ...)
-package cyphergraphexporter // import "github.com/clement-casse/playground/otelcol-custom/exporter/cyphergraphexporter"
--- otelcol-custom/exporter/cyphergraphexporter/internal/neo4jdriverwrap/doc.go
+++
@@ -1,4 +0,0 @@
-// Package neo4jdriverwrap holds the logic of adapting neo4jdriver options and functions
-// to be usable from the OpenTelemetry exporter module.
-// This module is made of helper functions.
-package neo4jdriverwrap
